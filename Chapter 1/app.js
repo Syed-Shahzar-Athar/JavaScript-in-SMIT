880c38a6ab02d09bf7a938b9872af9e47a832c45
Syed Shahzar Athar <155887635+Syed-Shahzar-Athar@users.noreply.github.com>
2024-02-29 15:34:47 +0500

    Add files via upload

---
+++ Chapter 1/app.js
@@ -0,0 +1,11 @@
+// Greet Message
+alert("Ans#1: Asalam o Alaikum");
+// Error MEssage
+alert("Ans#2: Error! Please Enter a valid password.");
+// Line Break
+alert("Ans#3: Welcome to my JS First Assignment\nI'm Enjoying to learn JavaScript")
+// Sequence Messages
+alert("Ans#4(i): Welcome to My Website")
+alert("Ans#4(ii): Happy Coding :)")
+// console message
+console.log("Ans#5: Hello! I can run JavaScript")
