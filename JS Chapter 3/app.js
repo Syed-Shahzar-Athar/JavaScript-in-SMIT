e885a016133527720fa55f1ad7685cc294456614
Syed Shahzar Athar <155887635+Syed-Shahzar-Athar@users.noreply.github.com>
2024-03-01 23:56:37 +0500

    Add files via upload

---
+++ JS Chapter 3/app.js
@@ -0,0 +1,23 @@
+// Declare a variable called age & assign to it your age. Show your age in an alert box.
+let age=(22);
+alert("I'm " +age +" years old");
+
+// Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser
+let birthYear=(2002);
+document.write("my birthyear is " +birthYear +" <br> Data type of my declared variable is number. <br>");
+
+
+// Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
+let visited = "11"
+alert("You have visited this website " +visited +" times")
+// Write a script to store in variables the following information: Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// Visitor's information
+let visitorName = "Syed Shahmeer";
+let productTitle = "T-shirt";
+let quantity = 5;
+
+// Message to display
+let message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on <b>Shah Jee Clothing Store</b>";
+
+// Display the message in the browser
+document.write(message);
